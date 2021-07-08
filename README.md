8b26d1bad109b06310c691f9325af93e266f82d0
faaabi93 <fabian.baiersdoerfer@gmx.de>
2021-07-08 19:39:19 +0200

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # library
+
+Creating a online library from scratch only with basic guidance.  
+Includes adding and removing books as well as marking them as read or not read.
+  
+Working version can be found [HERE](https://faaabi93.github.io/library/).
--- app.js
+++ app.js
@@ -1,28 +1,83 @@
 const gridContainer = document.querySelector(".grid-container");
+const addBtn = document.querySelector(".add-btn");
+const addBtnModal = document.querySelector(".modal-bottom");
+const modal = document.querySelector(".modal")
 
-myLibrary = []
-
-function Book(title, author, pages) {
+// Book
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
 };
+
+// Library:
+myLibrary = []
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
     createItem(book);
-    console.log(myLibrary);
 };
 
-function test() {
-    console.log("test");
-}
+function removeBookFromLibrary(e) {
+    var index = myLibrary.findIndex(x => x.title === e.path[1].firstChild.data);
+    myLibrary.splice(index, 1);
+    gridContainer.removeChild(e.path[2]);
+};
 
-function removeBookFromLibrary(book) {
-    /// var index = myLibrary.findIndex(x => x.title === book.title);
-    console.log("hallo");
-    //console.log(index);
-}
+function markBookAsRead(e) {
+    var index = myLibrary.findIndex(x => x.title === e.path[2].children[0].firstChild.data);
+    myLibrary[index].isRead ^= true;
+    if(e.path[1].children[2].innerText === "Not read yet") {
+        e.path[1].children[2].innerText = "Already read";
+    } else {
+        e.path[1].children[2].innerText = "Not read yet";
+    }
+};
+
+// Modal:
+addBtn.addEventListener("click", openModal);
+addBtnModal.addEventListener("click", addButtonModal);
+
+function openModal() {
+    modal.style.display="block";
+};
+
+function closeModal() {
+    modal.style.display="none";
+    clearModal();
+};
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        closeModal();
+        clearModal();
+    }
+};
+
+function addButtonModal() {
+    var author = document.getElementById("new-author").value;
+    var title = document.getElementById("new-title").value;
+    var pages = document.getElementById("new-pages").value;
+    if (!(author && title && pages) || (pages < 1)) return;
+    var isRead = document.getElementById("new-isRead").checked;
+    var tempBook = new Book(title, author, pages, isRead);
+    addBookWidget(tempBook);
+    closeModal();
+};
+
+function clearModal() {
+    document.getElementById("new-author").value = "";
+    document.getElementById("new-title").value = "";
+    document.getElementById("new-pages").value = "";
+
+};
+
+// UI
+function addBookWidget(book) {
+    var buch = new Book(book.title, book.author, book.pages, book.isRead);
+    addBookToLibrary(buch);
+};
 
 function createItems() {
     myLibrary.forEach(element => createItem(element));
@@ -33,29 +88,39 @@ function createItem(book) {
     div.classList.add("grid-item");
 
     let topDiv = document.createElement("div");
-    let deleteButton = document.createElement("div");
-    deleteButton.classList.add("red-btn", "round-s");
-    deleteButton.textContent = "✘";
     topDiv.classList.add("grid-item-top");
+
+    let deleteButton = document.createElement("div");
+    deleteButton.classList.add("red-btn", "round-s", "delete-btn");
+    deleteButton.textContent = "✘";
+    deleteButton.addEventListener("click", removeBookFromLibrary);
     topDiv.textContent = `${book.title}`;
     topDiv.appendChild(deleteButton);
 
     let bottomDiv = document.createElement("div");
     bottomDiv.classList.add("grid-item-bottom");
+    
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
-    p1.textContent = `Autor: ${book.author}`;
-    p2.textContent = `${book.pages} pages`;
-    p3.textContent = `ToDo`;
+    p1.innerHTML = `<b>Autor</b>: ${book.author}`;
+    p2.innerHTML = `<b>Pages</b>: ${book.pages}`;
+    if(book.isRead){
+        p3.textContent = "Already read";
+    } else {
+        p3.textContent = "Not read yet";
+    }
+    let changeReadBtn = document.createElement("div");
+    changeReadBtn.textContent = " ";
+    changeReadBtn.classList.add("blue-btn", "round-s", "change-btn")
+    changeReadBtn.addEventListener("click", markBookAsRead);
+
     bottomDiv.appendChild(p1);
     bottomDiv.appendChild(p2);
     bottomDiv.appendChild(p3);
+    bottomDiv.appendChild(changeReadBtn)
 
     div.appendChild(topDiv);
     div.appendChild(bottomDiv);
     gridContainer.appendChild(div);
-    console.log(book);
 };
-
-createItems();
